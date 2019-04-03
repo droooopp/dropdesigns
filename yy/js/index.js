d2085e0611f81dd05775e6cd602843319fa03641
Takayuki Suzuki <droooopp@gmail.com>
2019-04-03 19:10:42 +0900

    yy

--- yy/js/index.js
+++ yy/js/index.js
@@ -1,7 +1,144 @@
 (function($, window, document, undefined) {
   var __$win = $(window);
 
-  __heroGradation();
+  $(function() {
+    __menuDrawer();
+    __viewport();
+    __heroGradation();
+  });
+
+
+
+  /**
+   * ------------------------------------------------------------
+   * ドロワー実行
+   * ------------------------------------------------------------
+   */
+  function __menuDrawer() {
+
+    _init();
+
+
+
+    /* ----------------------------------------
+     * 初期化
+     * ---------------------------------------- */
+
+    function _init() {
+      _drawer();
+    }
+
+
+
+    /* ----------------------------------------
+     * ドロワー設定
+     * ---------------------------------------- */
+
+    function _drawer() {
+        var _$drawerTrigger = $('.header-action,.header-overlay');
+        var _$drawer = $('.header-nav');
+        var _$overlay = $('.header-overlay');
+        var _$body = $('body');
+
+        _$drawerTrigger
+          .click(function() {
+            if(!_$drawerTrigger.hasClass('header-action-close')) {
+              _$body
+                .addClass('is-showNav');
+              _$drawerTrigger
+                .addClass('header-action-current');
+              _$overlay
+                .fadeIn(300);
+              _$drawer
+                .css({
+                  top: 0
+                });
+              setTimeout(function(){
+                _$drawerTrigger
+                  .removeClass('header-action-current')
+                  .addClass('header-action-close');
+              },300);
+
+              if(_$drawer.outerHeight() <= __$win.height()) {
+                __$win
+                  .on('touchmove.noScroll', function(e) {
+                    e.preventDefault();
+                  });
+              }
+            }
+            else {
+              __$win
+                .off('.noScroll');
+              _$body
+                .removeClass('is-showNav');
+              _$drawer
+                .css({
+                  top: '-100vh'
+                });
+              _$drawerTrigger
+                .removeClass('header-action-close')
+                .addClass('header-action-current');
+              _$overlay
+                .fadeOut(300);
+              setTimeout(function(){
+                _$drawerTrigger
+                  .removeClass('header-action-current');
+              },300);
+            }
+          });
+
+      }
+
+
+  }
+
+
+  /**
+   * ------------------------------------------------------------
+   * ビューポート実行
+   * ------------------------------------------------------------
+   */
+  function __viewport() {
+
+    _init();
+
+
+
+    /* ----------------------------------------
+     * 初期化
+     * ---------------------------------------- */
+
+    function _init() {
+      _setViewport();
+    }
+
+
+
+    /* ----------------------------------------
+     * ビューポート設定
+     * ---------------------------------------- */
+
+    function _setViewport() {
+      var getDevice = (function(){
+        var ua = navigator.userAgent;
+        if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
+            return 'sp';
+        }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
+            return 'tab';
+        }else{
+            return 'other';
+        }
+      })();
+
+      if( getDevice == 'tab' ){
+        $viewport = $('meta[name="viewport"]');
+        $viewport
+          .attr({
+            content: 'width=1040'
+          });
+      }
+    }
+  }
 
 
 
@@ -21,9 +158,7 @@
      * ---------------------------------------- */
 
     function _init() {
-      $(function() {
-        _setGradation();
-      });
+      _setGradation();
     }
 
 
@@ -43,6 +178,41 @@
             }
         }
       });
+
+      $(".hero-circle").ripples({
+        resolution: 512,
+  			dropRadius: 20, //px
+  			perturbance: 0.04,
+      });
+
+
+      setInterval(function() {
+    		var $el = $('.hero-circle');
+    		var x = Math.random() * $el.outerWidth();
+    		var y = Math.random() * $el.outerHeight();
+    		var dropRadius = 20;
+    		var strength = 0.04 + Math.random() * 0.04;
+
+    		$el.ripples('drop', x, y, dropRadius, strength);
+    	}, 400);
+
+
+      $(".hero-koi").ripples({
+        resolution: 512,
+  			dropRadius: 20, //px
+  			perturbance: 0.04,
+      });
+
+
+      setInterval(function() {
+    		var $el = $('.hero-koi');
+    		var x = Math.random() * $el.outerWidth();
+    		var y = Math.random() * $el.outerHeight();
+    		var dropRadius = 20;
+    		var strength = 0.04 + Math.random() * 0.04;
+
+    		$el.ripples('drop', x, y, dropRadius, strength);
+    	}, 400);
     }
   }
 
